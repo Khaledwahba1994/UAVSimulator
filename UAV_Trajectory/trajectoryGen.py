6c2cc6d271c6ac216d63ec2fd10e5082bd653627
Khaled Wahba <khaled.hassan1994@gmail.com>
2021-10-06 19:55:46 +0200

    trying to fix problems with cvxpy

--- UAV_Trajectory/trajectoryGen.py
+++ UAV_Trajectory/trajectoryGen.py
@@ -6,27 +6,27 @@ import matplotlib.pyplot as plt
 polynomial = poly.Polynomial
 np.set_printoptions(linewidth=np.inf)
 np.set_printoptions(suppress=True)
-# fig = plt.figure(figsize=(10,10))
-# ax  = fig.add_subplot(autoscale_on=True,projection="3d")
-# ax.plot(data[1,:],data[2,:],data[3,:])
-# plt.show()
+# 
 
 # Problem data.
 # WayPoints for cirle trajetory
 r = 0.3
 height = 0.7
 w = 4 * np.pi
-T = 1 * 2 * np.pi / w
+T = 0.3 * (2 * np.pi / w)
 time = np.linspace(0,T,600)
 
 data = np.empty((4,time.size))
 data[0,:]  = time
 data[1,:]  = r * np.cos(w * time)
 data[2,:]  = r * np.sin(w * time)
-data[3,:]  = height#1+time/10
+data[3,:]  = 1+time/10
+fig = plt.figure(figsize=(10,10))
+ax  = fig.add_subplot(autoscale_on=True,projection="3d")
+ax.plot(data[1,:],data[2,:],data[3,:])
 
 
-pieces = 2
+pieces = 3
 n_waypoints = pieces + 1 #number of waypoints
 hk = time[-1]/pieces #time per piece [0,hk]
 
@@ -69,7 +69,7 @@ for axis in range(0,3):
     j,k = 0,3 
     for i in range(0,pieces-1):
         b_v[i,0] += one_vec @ invHk @ ((pk[axis,i+2] - pk[axis,i+1])*one_vec.T) \
-            - h1_vec @ invHk @ ((pk[axis,i+1] - pk[axis,i])*one_vec.T) 
+                  - h1_vec @ invHk @ ((pk[axis,i+1] - pk[axis,i])*one_vec.T) 
         Adiag  = (h1_vec @ invHk) + (one_vec @ invHk @ hk_tri) 
         A_v[i,j:j+3] = Adiag[:,1:]
         if i >= 1 and i < pieces-1:
@@ -79,7 +79,7 @@ for axis in range(0,3):
         if k+3 <= 3*(pieces-1):
             As0 = -one_vec @ invHk
             A_v[i,k:k+3] = As0[:,1:]
-            k += j   
+            k += 3   
     mid_conditions = la.pinv(A_v) @  b_v
     ind = 1
     for midIndex in range(0,len(mid_conditions),3):
@@ -105,13 +105,13 @@ for i in range(0,8*pieces,8):
 Ax_eq = np.zeros((8*pieces,8*pieces))
 Ay_eq = np.zeros((8*pieces,8*pieces))
 Az_eq = np.zeros((8*pieces,8*pieces))
-bx = np.zeros((8*pieces,))
-by = np.zeros((8*pieces,))
-bz = np.zeros((8*pieces,))
+bx = np.zeros((8*pieces,1)).reshape(8*pieces,1)
+by = np.zeros((8*pieces,1)).reshape(8*pieces,1)
+bz = np.zeros((8*pieces,1)).reshape(8*pieces,1)
 
 A0 = np.eye(4)
 A0[2,2] = 2 
-A0[3,3] = 6;
+A0[3,3] = 6
 
 for axis in range(0,3):
     for i in range(0,pieces):
@@ -120,8 +120,8 @@ for axis in range(0,3):
         b1  = np.array([[pk[axis,i+1] - pk[axis,i] - vk[axis,i]*hk - 0.5*ak[axis,i]*hk**2  - (1/6)*jk[axis,i]*hk**3],
                             [vk[axis,i+1] - vk[axis,i] - ak[axis,i]*hk - 0.5*jk[axis,i]*hk**2],
                                     [ak[axis,i+1] - ak[axis,i] - jk[axis,i]*hk],
-                                        [jk[axis,i+1] - jk[axis,i]]])
-        b = np.array([[b0],[b1]]).reshape(8,)
+                                        [jk[axis,i+1] - jk[axis,i]]]).reshape(4,1)
+        b = np.array([[b0],[b1]]).reshape(8,1)
         if axis == 0:
             Ax_eq[8*i:8*(i+1),8*i:8*(i+1)] = la.block_diag(A0,Hk)
             bx[8*i:8*(i+1)] = b
@@ -133,22 +133,85 @@ for axis in range(0,3):
             bz[8*i:8*(i+1)] = b
 
 # Construct the problem.
-n =  8 * pieces 
-print(n)
-print(Qx.shape)
-x = cp.Variable(n)
-print(x)
-# print('Qx = ')
-# print(Qx)
-# print('Ax_eq = ')
-# print(Ax_eq)
-# print('bx = ')
-# print(bx)
-prob1 = cp.Problem(cp.Minimize(x.T@Qx@x), 
-                    [Ax_eq @ x == bx])
+coefsx = np.zeros((pieces-1,8))
+coefsy = np.zeros((pieces-1,8))
+coefsz = np.zeros((pieces-1,8))
 
-print("prob1 is DCP:", prob1.is_dcp())
-print("Ax_eq@x == bx is DCP:", (Ax_eq@x == bx).is_dcp())
+j = 0
+n = 8 * pieces
+# for i in range(0,3):
+x = cp.Variable((n,1))
+y = cp.Variable((n,1))
+z = cp.Variable((n,1))
+print(Ax_eq)
+objectivex = cp.quad_form(x, Qx)
+objectivey = cp.quad_form(y, Qy) 
+objectivez = cp.quad_form(y, Qy)
+constraintsx = [Ax_eq@x == bx]
+constraintsy = [Ay_eq@y == by]
+constraintsz = [Az_eq@z == bz]
+
+probx = cp.Problem(cp.Minimize(cp.quad_form(x, Qx)),constraints=constraintsx)
+proby = cp.Problem(cp.Minimize(objectivex),constraintsy)
+probz = cp.Problem(cp.Minimize(objectivex),constraintsz)
+
+probx.solve(solver="OSQP",eps_dual_inf=1e-6)
+print("\nThe optimal value is", probx.value)
+print("A solution x is")
+print(x.value)
+
+print(y.value)
+
+# probz.solve()
+# plt.show()
+# y = cp.Variable(n)
+# proby = cp.Problem(cp.Minimize(cp.quad_form(y, Qy)),  [Ay_eq @ y == by])
+# print("proby is DCP:", proby.is_dcp())
+# print("Ay_eq@y == by is DCP:", (Ay_eq@y == by).is_dcp())
+# z = cp.Variable(n)
+# probz = cp.Problem(cp.Minimize(cp.quad_form(z, Qz)),  [Az_eq @ z == bz])
+# print("probz is DCP:", probz.is_dcp())
+# print("Az_eq@z == bz is DCP:", (Az_eq@z == bz).is_dcp())
+# for i in range(0,8*pieces,8):
+    
+#     Qxi = Qx[i:i+8,i:i+8].reshape(8,8)
+#     Axi_eq = Ax_eq[i:i+8,i:i+8].reshape(8,8)
+#     bxi = bx[i:i+8].reshape(8,1)
+
+#     x = cp.Variable((n,1))
+#     probx = cp.Problem(cp.Minimize(cp.quad_form(x, Qxi)),  [Axi_eq @ x == bxi])
+#     # probx.solve()
+#     # coefsx[j,0:8] = x.value.reshape(n,)
+#     print("probx is DCP:", probx.is_dcp())
+#     # print("Axi_eq@x == bxi is DCP:", (Axi_eq@x == bxi).is_dcp())
+
+#     Qyi = Qy[i:i+8,i:i+8].reshape(8,8)
+#     Ayi_eq = Ay_eq[i:i+8,i:i+8].reshape(8,8)
+#     byi = by[i:i+8].reshape(8,1)
+
+#     y = cp.Variable((n,1))
+#     proby = cp.Problem(cp.Minimize(cp.quad_form(y, Qyi)), [Ayi_eq @ y == byi])
+#     print("proby is DCP:", proby.is_dcp())
+#     # print("Ayi_eq@x == byi is DCP:", (Ayi_eq@x == byi).is_dcp())
+#     # proby.solve()
+#     # coefsy[j,0:8] = y.value.reshape(n,)
+
+#     Qzi = Qz[i:i+8,i:i+8].reshape(8,8)
+#     Azi_eq = Az_eq[i:i+8,i:i+8].reshape(8,8)
+#     bzi = bz[i:i+8].reshape(8,1)
+
+#     z = cp.Variable((n,1))
+#     probz = cp.Problem(cp.Minimize(cp.quad_form(z, Qzi)), [Azi_eq @ z == bzi])
+#     print("probz is DCP:", probz.is_dcp())
+#     # print("Azi_eq@x == bzi is DCP:", (Azi_eq@x == bzi).is_dcp())
+#     # probz.solve()
+#     # coefsz[j,0:8] = z.value.reshape(n,)
+#     j += 1
+# print(coefsx)
+# print(coefsy)
+# print(coefsz)
+# print("probx is DCP:", probx.is_dcp())
+# print("Ax_eq@x == bx is DCP:", (Ax_eq@x == bx).is_dcp())
 
 # prob1.solve()
 # print("\nThe optimal value is", prob1.value)
