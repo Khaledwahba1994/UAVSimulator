9a775c20d2f856415dc9972222e15ab3db3dae7d
Khaled Wahba <khaled.hassan1994@gmail.com>
2021-10-11 20:33:15 +0200

    revise the b_v vector

--- UAV_Trajectory/trajectoryGen.py
+++ UAV_Trajectory/trajectoryGen.py
@@ -13,21 +13,21 @@ np.set_printoptions(suppress=True)
 # WayPoints for cirle trajetory
 r = 0.3
 height = 0.7
-w = 4 * np.pi
-T = 0.3 * (2 * np.pi / w)
-time = np.linspace(0,T,600)
+w = 2 * np.pi
+T = 2 * (2 * np.pi / w)
+time = np.linspace(0,T,100,endpoint=True)
 
 data = np.empty((4,time.size))
 data[0,:]  = time
 data[1,:]  = r * np.cos(w * time)
 data[2,:]  = r * np.sin(w * time)
-data[3,:]  = 1+time/10
+data[3,:]  = height#1+time/10
 fig = plt.figure(figsize=(10,10))
 ax  = fig.add_subplot(autoscale_on=True,projection="3d")
 ax.plot(data[1,:],data[2,:],data[3,:])
 
 
-pieces = 10
+pieces = 5
 n_waypoints = pieces + 1 #number of waypoints
 hk = time[-1]/pieces #time per piece [0,hk]
 
@@ -37,11 +37,10 @@ vk   = np.zeros((3,n_waypoints))
 ak   = np.zeros((3,n_waypoints))
 jk   = np.zeros((3,n_waypoints))
 step = round((len(data.T))/pieces)
-
+print(step)
 for i in range(0,pieces):
     index  = step*(i)
     pk[:,i] = data[1:,index]
-
 pk[:,-1] = data[1:,-1]
 
 
@@ -61,16 +60,21 @@ for axis in range(0,3):
                     [0, 0,     0     ,          1]]).reshape(4,4)
     invHk = la.inv(Hk)
     one_vec = np.array([1,0,0,0]).reshape(1,4)
+
     vars0 = np.array([0,vk[axis,0],ak[axis,0],jk[axis,0]]).reshape(4,1)
     varsN = np.array([0,vk[axis,-1],ak[axis,-1],jk[axis,-1]]).reshape(4,1)
+
     A_v = np.zeros((pieces-1,3*(pieces-1)))
     b_v = np.zeros((pieces-1,1))
-    b_v[0,0] = (h1_vec @ invHk @ hk_tri @ vars0)
-    b_v[-1,0] =  np.array([1,0,0,0]) @ invHk @varsN
+
+    b_v[0,0]  = h1_vec @ invHk @ hk_tri @ vars0
     j,k = 0,3 
+
     for i in range(0,pieces-1):
-        b_v[i,0] += one_vec @ invHk @ ((pk[axis,i+2] - pk[axis,i+1])*one_vec.T) \
-                  - h1_vec @ invHk @ ((pk[axis,i+1] - pk[axis,i])*one_vec.T) 
+        ## I need to revise this vector, calculation-wise
+        b_v[pieces-i-2,0] +=  -( (one_vec @ invHk @ ((pk[axis,i+2] - pk[axis,i+1])*one_vec.T)) \
+                              -  (h1_vec @ invHk @ ((pk[axis,i+1] -   pk[axis,i])*one_vec.T)))
+
         Adiag  = (h1_vec @ invHk) + (one_vec @ invHk @ hk_tri) 
         A_v[i,j:j+3] = Adiag[:,1:]
         if i >= 1 and i < pieces-1:
@@ -80,8 +84,14 @@ for axis in range(0,3):
         if k+3 <= 3*(pieces-1):
             As0 = -one_vec @ invHk
             A_v[i,k:k+3] = As0[:,1:]
-            k += 3   
+            k += 3       
+    print('A_v')
+    print(A_v)
+    print('b_v')
+    print(b_v.T)         
     mid_conditions = la.pinv(A_v) @  b_v
+    print()
+    print(mid_conditions)
     ind = 1
     for midIndex in range(0,len(mid_conditions),3):
         vk[axis,ind] = mid_conditions[midIndex]
@@ -90,6 +100,9 @@ for axis in range(0,3):
         ind += 1
 
 # print('vk = ') / print(vk) /print('ak' ) /print(ak) / print('jk ') /print(jk)
+vk   = np.zeros((3,n_waypoints))
+ak   = np.zeros((3,n_waypoints))
+jk   = np.zeros((3,n_waypoints))
 ## Construct the Hessian Matrix Q
 Qx = np.eye(8*pieces)
 Qy = np.eye(8*pieces)
@@ -106,15 +119,15 @@ for i in range(0,8*pieces,8):
 Ax_eq = np.zeros((8*pieces,8*pieces))
 Ay_eq = np.zeros((8*pieces,8*pieces))
 Az_eq = np.zeros((8*pieces,8*pieces))
-bx = np.zeros((8*pieces,1)).reshape(8*pieces,1)
-by = np.zeros((8*pieces,1)).reshape(8*pieces,1)
-bz = np.zeros((8*pieces,1)).reshape(8*pieces,1)
+bx = np.zeros((8*pieces,))
+by = np.zeros((8*pieces,))
+bz = np.zeros((8*pieces,))
 
 A0 = np.eye(4)
 A0[2,2] = 2 
 A0[3,3] = 6
-
-for axis in range(0,3):
+print(pk)
+for axis in range(0,1):
     for i in range(0,pieces):
         b0 = np.array([[pk[axis,i], vk[axis,i], ak[axis,i], jk[axis,i]]]).reshape(4,1);
 
@@ -122,7 +135,8 @@ for axis in range(0,3):
                             [vk[axis,i+1] - vk[axis,i] - ak[axis,i]*hk - 0.5*jk[axis,i]*hk**2],
                                     [ak[axis,i+1] - ak[axis,i] - jk[axis,i]*hk],
                                         [jk[axis,i+1] - jk[axis,i]]]).reshape(4,1)
-        b = np.array([[b0],[b1]]).reshape(8,1)
+
+        b = np.array([[b0],[b1]]).reshape(8,)
         if axis == 0:
             Ax_eq[8*i:8*(i+1),8*i:8*(i+1)] = la.block_diag(A0,Hk)
             bx[8*i:8*(i+1)] = b
@@ -138,33 +152,53 @@ coefsx = np.zeros((pieces-1,8))
 coefsy = np.zeros((pieces-1,8))
 coefsz = np.zeros((pieces-1,8))
 
-j = 0
-n = 8 * pieces
-for i in range(0,n,8):
-    x = cp.Variable((8,1))
-    y = cp.Variable((8,1))
-    z = cp.Variable((8,1))
 
-    objectivex = cp.quad_form(x, Qx[i:i+8,i:i+8])
-    objectivey = cp.quad_form(y, Qy[i:i+8,i:i+8]) 
-    objectivez = cp.quad_form(z, Qz[i:i+8,i:i+8])
-    constraintsx = [Ax_eq[i:i+8,i:i+8]@x == bx[i:i+8]]
-    constraintsy = [Ay_eq[i:i+8,i:i+8]@y == by[i:i+8]]
-    constraintsz = [Az_eq[i:i+8,i:i+8]@z == bz[i:i+8]]
+n = 8 * pieces
+
+for i in range(0,n,8):
+    x = cp.Variable(8)
+    y = cp.Variable(8)
+    z = cp.Variable(8)
+
+    objectivex = (1/2)*cp.quad_form(x, Qx[i:i+8,i:i+8])
+    objectivey = (1/2)*cp.quad_form(y, Qy[i:i+8,i:i+8]) 
+    objectivez = (1/2)*cp.quad_form(z, Qz[i:i+8,i:i+8])
+
+    constraintsx = [Ax_eq[i:i+8,i:i+8] @ x == bx[i:i+8]]
+    constraintsy = [Ay_eq[i:i+8,i:i+8] @ y == by[i:i+8]]
+    constraintsz = [Az_eq[i:i+8,i:i+8] @ z == bz[i:i+8]]
 
     probx = cp.Problem(cp.Minimize(objectivex),constraints=constraintsx)
     proby = cp.Problem(cp.Minimize(objectivey),constraints=constraintsy)
     probz = cp.Problem(cp.Minimize(objectivez),constraints=constraintsz)
+
+    print(bx[i:i+8])
+
     print("probx is DCP:", probx.is_dcp())
     print("proby is DCP:", proby.is_dcp())
     print("probz is DCP:", probz.is_dcp())
+    print()
     print("Ax_eq@x == bx is DCP:", (constraintsx[0]).is_dcp())
     print("Ay_eq@x == by is DCP:", (constraintsy[0]).is_dcp())
     print("Az_eq@x == bz is DCP:", (constraintsz[0]).is_dcp())
-    probx.solve(solver='OSQP',enforce_dpp=True)
-    print("\nThe optimal value is", probx.value)
-    print("A solution x is")
-    print(x.value)
+
+
+# probx.solve()
+# print("\nThe optimal value is", probx.value)
+# print("A solution x is")
+# print(x.value)
+
+# proby.solve()
+# print("\nThe optimal value is", proby.value)
+# print("A solution y is")
+# print(y.value)
+
+# probz.solve()
+# print("\nThe optimal value is", probz.value)
+# print("A solution z is")
+# print(z.value)
+
+
 # probx.solve(solver='OSQP',enforce_dpp=True)
 # print("\nThe optimal value is", probx.value)
 # print("A solution x is")
